2aab9629f4df41e03a28c43dc7e22ab797a6a252
Mikhail Kabanov <kabanov.mike@gmail.com>
2019-10-12 01:41:27 +1000

    Feed daily filter

--- server/controllers/feed.controller.js
+++ server/controllers/feed.controller.js
@@ -14,16 +14,25 @@ async function list(req, res, next) {
     }
 
     const subscriptions = await getFriendList(userId);
-    const { page = 1 } = req.query;
-    const options = poemsQuery(page, {
+    const { page = 1, daily } = req.query;
+    const query = {
         user_id: userId,
         friends: subscriptions,
         where: {
             user_id: { [Op.in]: subscriptions },
             is_active: true
         }
-    });
+    };
 
+    /**
+     * Daily poem filter
+     */
+    if (daily) {
+        query.where.daily_id = Number(daily);
+        delete query.where.user_id;
+    }
+
+    const options = poemsQuery(page, query);
     Poem.increment('views_count', { where: { ...options.where, user_id: { [Op.not]: userId } } });
 
     findWithPaginate(Poem, options)
